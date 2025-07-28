1c4e664e03281c91c95853383176fe106b90415f
Sahas Subramanian <sahas.subramanian@proton.me>
2025-07-28 15:42:14 +0200

    Add a `Formula` trait exposing generic formula operations
    This just exposes the same methods from the component graph's Formula
    trait, but with metric comparison and retaining the ability to
    subscribe to them.
    
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

--- src/logical_meter/formula.rs
+++ src/logical_meter/formula.rs
@@ -3,6 +3,7 @@
 
 //! Formula module for the logical meter.
 
+use frequenz_microgrid_component_graph::Formula as _;
 mod aggregation_formula;
 mod coalesce_formula;
 pub(crate) mod graph_formula_provider;
@@ -42,5 +43,71 @@ impl<F: GraphFormulaProvider> FormulaParams<F> {
             metric,
             instructions_tx,
         }
+    }
+}
+
+/// A trait that defines generic formula operations.
+pub trait Formula: std::fmt::Display + Sized {
+    fn coalesce(self, other: Self) -> Result<Self, Error>;
+    fn min(self, other: Self) -> Result<Self, Error>;
+    fn max(self, other: Self) -> Result<Self, Error>;
+    fn subscribe(&self) -> impl Future<Output = Result<broadcast::Receiver<Sample>, Error>> + Send;
+}
+
+impl<T> Formula for T
+where
+    T: FormulaSubscriber
+        + GraphFormulaProvider
+        + From<FormulaParams<T>>
+        + Into<FormulaParams<T>>
+        + std::fmt::Display,
+{
+    fn coalesce(self, other: Self) -> Result<Self, Error> {
+        let mut params_self: FormulaParams<T> = self.into();
+        let params_other: FormulaParams<T> = other.into();
+
+        if params_self.metric != params_other.metric {
+            return Err(Error::invalid_metric(format!(
+                "Cannot coalesce formulas with different metrics: {} and {}",
+                params_self.metric.as_str_name(),
+                params_other.metric.as_str_name()
+            )));
+        }
+        params_self.formula = params_self.formula.coalesce(params_other.formula);
+        Ok(params_self.into())
+    }
+
+    fn min(self, other: Self) -> Result<Self, Error> {
+        let mut params_self: FormulaParams<T> = self.into();
+        let params_other: FormulaParams<T> = other.into();
+
+        if params_self.metric != params_other.metric {
+            return Err(Error::invalid_metric(format!(
+                "Cannot take min of formulas with different metrics: {} and {}",
+                params_self.metric.as_str_name(),
+                params_other.metric.as_str_name()
+            )));
+        }
+        params_self.formula = params_self.formula.min(params_other.formula);
+        Ok(params_self.into())
+    }
+
+    fn max(self, other: Self) -> Result<Self, Error> {
+        let mut params_self: FormulaParams<T> = self.into();
+        let params_other: FormulaParams<T> = other.into();
+
+        if params_self.metric != params_other.metric {
+            return Err(Error::invalid_metric(format!(
+                "Cannot take max of formulas with different metrics: {} and {}",
+                params_self.metric.as_str_name(),
+                params_other.metric.as_str_name()
+            )));
+        }
+        params_self.formula = params_self.formula.max(params_other.formula);
+        Ok(params_self.into())
+    }
+
+    fn subscribe(&self) -> impl Future<Output = Result<broadcast::Receiver<Sample>, Error>> + Send {
+        <T as FormulaSubscriber>::subscribe(self)
     }
 }
