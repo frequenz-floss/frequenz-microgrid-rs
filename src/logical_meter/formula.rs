112c2002236b828cdc39ddab76d89e6f6353b0ff
Sahas Subramanian <sahas.subramanian@proton.me>
2025-08-25 18:31:32 +0200

    Update Formula trait to support generic Quantity type
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

--- src/logical_meter/formula.rs
+++ src/logical_meter/formula.rs
@@ -10,7 +10,7 @@ pub(crate) mod graph_formula_provider;
 pub use aggregation_formula::AggregationFormula;
 pub use coalesce_formula::CoalesceFormula;
 
-use crate::{Error, Sample, proto::common::v1alpha8::metrics::Metric};
+use crate::{Error, Sample, proto::common::v1alpha8::metrics::Metric, quantity::Quantity};
 use tokio::sync::{broadcast, mpsc};
 
 use super::logical_meter_actor;
@@ -47,20 +47,21 @@ impl<F: GraphFormulaProvider> FormulaParams<F> {
 }
 
 /// A trait that defines generic formula operations.
-pub trait Formula: std::fmt::Display + Sized {
+pub trait Formula<Q: Quantity = f32>: std::fmt::Display + Sized {
     fn coalesce(self, other: Self) -> Result<Self, Error>;
     fn min(self, other: Self) -> Result<Self, Error>;
     fn max(self, other: Self) -> Result<Self, Error>;
     fn subscribe(&self) -> impl Future<Output = Result<broadcast::Receiver<Sample>, Error>> + Send;
 }
 
-impl<T> Formula for T
+impl<T, Q> Formula<Q> for T
 where
     T: FormulaSubscriber
         + GraphFormulaProvider
         + From<FormulaParams<T>>
         + Into<FormulaParams<T>>
         + std::fmt::Display,
+    Q: Quantity,
 {
     fn coalesce(self, other: Self) -> Result<Self, Error> {
         let mut params_self: FormulaParams<T> = self.into();
